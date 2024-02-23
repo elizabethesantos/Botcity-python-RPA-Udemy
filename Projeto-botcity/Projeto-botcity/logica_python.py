da1693b4177113f207e58075e1a8bf2de88a4bf2
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2024-02-23 14:35:36 -0300

    logica botcity

---
+++ Projeto-botcity/Projeto-botcity/logica_python.py
@@ -0,0 +1,38 @@
+numero = 10
+texto = "Charles"
+verdade = True
+
+# print(numero, texto, verdade)
+print('Numero: ' +str(numero), f' Texto: {texto}',f' Verdade: {str(verdade)}' )
+# print(type(numero), type(texto), type(verdade))
+
+if texto != 'João':
+    print(texto)
+    print(numero)
+else:
+    texto = 'Pedro'
+    numero = 12
+    print(texto)
+    print(numero)
+
+funcionarios = ['Pedro', "Charles", 'Maria']
+
+for item in funcionarios:
+    print(item)
+
+while numero <= 20:
+    print(numero)
+    numero = numero +1
+
+def frase():
+    print('Curso de RPA!')
+
+frase()
+
+def calculaNumero(num1, num2):
+    resultado = num1 + num2
+    # print(resultado)
+    return resultado
+
+valor = calculaNumero(2, 5)
+print(valor)
